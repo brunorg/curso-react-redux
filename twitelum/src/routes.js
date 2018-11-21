9cfbc3b9756d6469b17d33803f60b23b36e09f6e
Bruno Roberto Gonçalves <bruno_rg@yahoo.com.br>
2018-11-21 20:36:55 -0200

    Criando router privado

--- twitelum/src/routes.js
+++ twitelum/src/routes.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Page404 from './pages/Page404';
 
+class PrivateRoute extends React.Component {
+    render() {
+        if (localStorage.getItem('TOKEN')) {
+            return (
+                <Route {...this.props} />
+            )
+        } else {
+            return <Redirect to="/login" />
+        }
+    }
+}
+
 const Routes = () => {
     return (
         <Switch>
-            <Route path="/" exact component={HomePage} />
+            <PrivateRoute path="/" exact component={HomePage} />
             <Route path="/login" component={LoginPage} />
             <Route component={Page404} />
         </Switch>
