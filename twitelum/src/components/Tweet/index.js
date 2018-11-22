7c21f6e9882315c2d057798628c23d3a84ab4e5f
Bruno Roberto Gonçalves <bruno_rg@yahoo.com.br>
2018-11-22 20:23:47 -0200

    Mudar estado dos likes para após ação do click no like, props > state

--- twitelum/src/components/Tweet/index.js
+++ twitelum/src/components/Tweet/index.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import './tweet.css'
 
 class Tweet extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            likeado: props.likeado,
+            totalLikes: props.totalLikes
+        }
+    }
+
+    like = () => {
+        const { likeado, totalLikes } = this.state;
+        this.setState({
+            likeado: !likeado,
+            totalLikes: (likeado ? totalLikes - 1 : totalLikes + 1)
+        })
+    }
+
     render() {
         return (
             <article className="tweet">
@@ -17,9 +33,10 @@ class Tweet extends Component {
                     <button className="btn btn--clean">
                         <svg className={`
                                 icon icon--small iconHeart
-                                ${this.props.likeado
+                                ${this.state.likeado
                                 ? 'iconHeart--active'
-                                : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.5 47.5">
+                                : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.5 47.5"
+                            onClick={this.like} >
                             <defs>
                                 <clipPath id="a">
                                     <path d="M0 38h38V0H0v38z"></path>
@@ -29,7 +46,7 @@ class Tweet extends Component {
                                 <path d="M36.885 25.166c0 5.45-4.418 9.868-9.867 9.868-3.308 0-6.227-1.632-8.018-4.128-1.79 2.496-4.71 4.129-8.017 4.129-5.45 0-9.868-4.418-9.868-9.868 0-.773.098-1.52.266-2.242C2.75 14.413 12.216 5.431 19 2.965c6.783 2.466 16.249 11.448 17.617 19.96.17.721.268 1.47.268 2.241"></path>
                             </g>
                         </svg>
-                        {this.props.totalLikes}
+                        {this.state.totalLikes}
                     </button>
                 </footer>
             </article>
