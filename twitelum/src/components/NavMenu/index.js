54bc6ac15569ae38a068520c420c19ba61b77e03
Bruno Roberto Gonçalves <bruno_rg@yahoo.com.br>
2018-11-21 22:53:31 -0200

    usando o decorator withRouter para acessar o history e fazer um redirect

--- twitelum/src/components/NavMenu/index.js
+++ twitelum/src/components/NavMenu/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import './navMenu.css'
 
 class NavMenu extends Component {
+
+    fazLogout = (event) => {
+        event.preventDefault()
+        localStorage.removeItem('TOKEN')
+        this.props.history.push('/login')
+    }
+
     render() {
         return (
             <nav className="navMenu">
@@ -19,7 +27,7 @@ class NavMenu extends Component {
                     <a className="navMenu__link" href="/">Hashtags</a>
                 </li>
                 <li className="navMenu__item">
-                    <a className="navMenu__link" href="/">Logout</a>
+                    <a className="navMenu__link" href="/" onClick={this.fazLogout} >Logout</a>
                 </li>
                 </ul>
             </nav>
@@ -27,4 +35,4 @@ class NavMenu extends Component {
     }
 }
 
-export default NavMenu
+export default withRouter(NavMenu)
